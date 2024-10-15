da2eb22a431ad737a283471ff4fb0e534cb45e9e
kdmkjm <kdmkjm@naver.com>
2024-10-15 23:44:36 +0900

    Create 12_6_2.cpp

---
+++ test_realize/12_6_2.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
+    vector<vector<int>> answer;
+    return answer;
+}
+
+/*
+8
+1 0 0 1
+1 1 1 1
+2 1 0 1
+2 2 1 1
+5 0 0 1
+5 1 0 1
+4 2 1 1
+3 2 1 1
+
+10
+0 0 0 1
+2 0 0 1
+4 0 0 1
+0 1 1 1
+1 1 1 1
+2 1 1 1
+3 1 1 1
+2 0 0 0
+1 1 1 0
+2 2 0 1
+
+
+*/
