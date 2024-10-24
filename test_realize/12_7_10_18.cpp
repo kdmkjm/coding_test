7a1a11ce1860b11f07034485edabafbcb150ce40
kdmkjm <kdmkjm@naver.com>
2024-10-25 02:02:20 +0900

    2024102503

--- test_realize/12_7_10_18.cpp
+++ test_realize/12_7_10_18.cpp
@@ -8,19 +8,20 @@ int n, m;
 vector<pair<int,int> > house;
 vector<pair<int,int> > chicken;
 
-int calcSum(vector<pair<int, int> > now)
+int calcDistance(vector<pair<int,int> > now)
 {
     int result = 0;
     for(int i = 0; i < house.size(); i++)
     {
+        int tmp = INF;
         int hx = house[i].first;
         int hy = house[i].second;
-        int tmp = INF;
         for(int j = 0; j < now.size(); j++)
         {
-            int nowx = now[j].first;
-            int nowy = now[j].second;
-            tmp = min(tmp, abs(nowx - hx) + abs(nowy - hy));
+            int nx = now[j].first;
+            int ny = now[j].second;
+            int distance = abs(nx-hx) + abs(ny-hy);
+            tmp = min(tmp, distance);
         }
         result += tmp;
     }
@@ -30,25 +31,21 @@ int calcSum(vector<pair<int, int> > now)
 int main()
 {
     cin >> n >> m;
-    for(int i = 1; i <= n; i++)
+    for(int i = 0; i < n; i++)
     {
-        for(int j = 1; j <= n; j++)
+        for(int j = 0; j < n; j++)
         {
             int a;
             cin >> a;
             if(a == 1)  house.push_back(make_pair(i,j));
-            else if(a == 2) chicken.push_back(make_pair(i,j));
+            if(a == 2)  chicken.push_back(make_pair(i,j));
         }
     }
 
-    int result = INF;
-
     vector<bool> v(chicken.size());
-//    fill(v.begin(), v.begin() + m, true);
-    for(int i = 0; i < m; i++)
-    {
-        v[i] = true;
-    }
+    fill(v.begin(), v.begin()+m, true);
+
+    int result = INF;
 
     do
     {
@@ -57,13 +54,10 @@ int main()
         {
             if(v[i])
             {
-                int vx = chicken[i].first;
-                int vy = chicken[i].second;
-                now.push_back(make_pair(vx,vy));
+                now.push_back(make_pair(chicken[i].first,chicken[i].second));
             }
         }
-        result = min(result, calcSum(now));
-    }while(prev_permutation(v.begin(), v.end()));
-
+        result = min(result, calcDistance(now));
+    } while (prev_permutation(v.begin(), v.end()));
     cout << result << '\n';
 }
