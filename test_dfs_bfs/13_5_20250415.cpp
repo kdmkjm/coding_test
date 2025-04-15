a6f919b1a912ee80e9928a1e273f651cb22a4d72
kdmkjm <kdmkjm0@gmail.com>
2025-04-15 23:50:04 +0900

    Update 13_5_20250415.cpp

--- test_dfs_bfs/13_5_20250415.cpp
+++ test_dfs_bfs/13_5_20250415.cpp
@@ -2,65 +2,64 @@
 #include <vector>
 using namespace std;
 
-int n;
+int n, max_value, min_value;
 vector<int> num;
 vector<int> sign;
-int max_value, min_value;
+
 void dfs(int i, int now)
 {
-    if(i == n)
-    {
-        max_value = max(max_value, now);
-        min_value = min(min_value, now);
-    }
-    else
-    {
-        if(sign[0] > 0)
-        {
-            sign[0]--;
-            dfs(i + 1, now + num[i]);
-            sign[0]++;
-        }
-        if(sign[1] > 0)
-        {
-            sign[1]--;
-            dfs(i + 1, now - num[i]);
-            sign[1]++;
-        }
-        if(sign[2] > 0)
-        {
-            sign[2]--;
-            dfs(i + 1, now * num[i]);
-            sign[2]++;
-        }
-        if(sign[3] > 0)
-        {
-            sign[3]--;
-            dfs(i + 1, now / num[i]);
-            sign[3]++;
-        }
-    }
+	if (i == n)
+	{
+		max_value = max(now, max_value);
+		min_value = min(now, min_value);
+	}
+	if (sign[0] > 0)
+	{
+		sign[0]--;
+		dfs(i + 1, now + num[i]);
+		sign[0]++;
+	}
+	if (sign[1] > 0)
+	{
+		sign[1]--;
+		dfs(i + 1, now - num[i]);
+		sign[1]++;
+	}
+	if (sign[2] > 0)
+	{
+		sign[2]--;
+		dfs(i + 1, now * num[i]);
+		sign[2]++;
+	}
+	if (sign[3] > 0)
+	{
+		sign[3]--;
+		dfs(i + 1, now / num[i]);
+		sign[3]++;
+	}
 }
+
 int main()
 {
-    cin >> n;
-    for(int i = 0; i < n; i++)
-    {
-        int tmp = 0;
-        cin >> tmp;
-        num.push_back(tmp);
-    }
-    for(int i = 0; i < 4; i++)
-    {
-        int tmp = 0;
-        cin >> tmp;
-        sign.push_back(tmp);
-    }
-    max_value = -1e9;
-    min_value = 1e9;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int tmp;
+		cin >> tmp;
+		num.push_back(tmp);
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		int tmp;
+		cin >> tmp;
+		sign.push_back(tmp);
+	}
 
-    dfs(1, num[0]);
+	max_value = -1e9;
+	min_value = 1e9;
 
-    cout << max_value << '\n';
-    cout << min_value << '\n';
+	dfs(1, num[0]);
+
+	cout << max_value << '\n';
+	cout << min_value << '\n';
 }
